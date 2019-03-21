63138b0bf94ad012dfce1beb712034f594e9c299
Jinpyo_K <39121365+maniaKj@users.noreply.github.com>
2019-03-22 06:12:24 +0900

    string 의 erase 구현 해봄

--- stringerase.cpp
+++ stringerase.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <string>
-#include <stdio.h>
-using namespace std;
+#define MAX 100
 
-void erase(char* str, char ch) {
+void erase(char* str, const char ch) {
 	int i = 0, j = 0;
 
 	while (str[i] != '\0') {
@@ -27,21 +26,25 @@ void erase(char* str, char ch) {
 }
 
 
+int test(const char * str, const char ch) {
+	char * c = new char[MAX];
+	
+	sprintf_s(c, MAX, str);
+
+	std::cout << c << " , len : " << strlen(c) << std::endl << " ==> ";
+	erase(c, ch);
+	std::cout << c << ", len : " << strlen(c) << std::endl;
+
+	delete c;
+	return 0;
+}
+
 int main(void) {
-	string s = "asdfasdf";
-
-	s = "hellohellohello";
-	cout << s << " , len : " << s.length() << ", after : ";
-	erase(&s[0], 'e');
-	cout << s << ", len : " << s.length() << endl;
-
-	s = "aaaaaaaaaaaaaa";
-	cout << s << " , len : " << s.length() << ", after : ";
-	erase(&s[0], 'b');
-	cout << s << ", len : " << s.length() << endl;
+	test("hellohellohello", 'e');
+	test("The quick brown dog jumps over the lazy fox", 'o');
+	test("Copies the null-terminated byte string pointed to by src,", 'i');
 
 
 	system("pause");
-
 	return 0;
 }
