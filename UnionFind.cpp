549d60b73f9766c57025204e7df1a512e0a08692
Jinpyo_K <39121365+maniaKj@users.noreply.github.com>
2019-05-15 13:42:46 +0900

    Update UnionFind.cpp

--- UnionFind.cpp
+++ UnionFind.cpp
@@ -1,3 +1,5 @@
+//array 말고 vector 활용 버전은 아래 있음
+
 #include <cstdio>
 #include <cassert>
 #include <vector>
@@ -63,4 +65,66 @@ int main( void ) {
     assert(root(1) == root(4));
 
     return 0;
+}
+
+----------------------------------------------------------
+    //vector 
+#include <string>
+#include <vector>
+#include <iostream>
+#include <map>
+#include <set>
+
+using namespace std;
+
+/* Find root of set containing x */
+int root(int x, vector<int> & uf) {
+	int r = x;
+
+	/* Find root */
+	while (uf[r] >= 0)
+		r = uf[r];
+
+	/* Compress path to root */
+	while (uf[x] >= 0) {
+		int tmp = uf[x];
+		uf[x] = r;
+		x = tmp;
+	}
+
+	return r;
+}
+
+/* Union of sets containing x and y */
+void join(int x, int y, vector<int> & uf) {
+	x = root(x, uf);
+	y = root(y, uf);
+
+	if (x != y) {
+		if (uf[x] < uf[y]) {
+			uf[x] += uf[y];
+			uf[y] = x;
+		}
+		else {
+			uf[y] += uf[x];
+			uf[x] = y;
+		}
+	}
+}
+
+int solution(int n, vector<vector<int>> computers) {
+    vector<int> uf(n, -1);
+    
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (i == j) continue;
+			if (computers[i][j] == 0) continue;
+			join(i, j, uf);
+		}
+	}
+
+    set<int> s;
+	for (int i = 0; i < n; i++) s.insert(root(i, uf));
+
+	return s.size();
 }
