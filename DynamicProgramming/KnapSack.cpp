66d77e0fe472c8acbbb0f676859e95e376d7fc38
Jinpyo_K <39121365+maniaKj@users.noreply.github.com>
2019-05-23 12:55:01 +0900

    Update KnapSack.cpp

--- DynamicProgramming/KnapSack.cpp
+++ DynamicProgramming/KnapSack.cpp
@@ -1,3 +1,38 @@
+//3학년 알고리즘 과제
+//코드 왜이러냐...
+struct packObj {
+	int value;
+	int weight;
+	float vpw;
+};
+
+int knapsack(packObj *many, int packVolume, int capability) {
+	int **k, i, j;
+	k = (int**)malloc(sizeof(int*) *packVolume);
+	for (i = 0; i < packVolume; i++) k[i] = (int*)malloc(sizeof(int)*capability+1);
+	for (i = 0; i < packVolume; i++) {
+		for (j = 0; j < capability+1; j++) {
+			k[i][j] = 0;
+		}
+	}
+
+	for (j = 1; j <= capability; j++) if (many[0].weight <= j) k[0][j] = many[0].value;
+	for (i = 1; i < packVolume; i++) {
+		for (j = 1; j <= capability; j++) {
+			if (many[i].weight > j) k[i][j] = k[i - 1][j];
+			else {
+					if (k[i - 1][j] > k[i - 1][j - many[i].weight] + many[i].value) 
+						k[i][j] = k[i - 1][j];
+					else 
+						k[i][j] = k[i - 1][j - many[i].weight] + many[i].value;
+			}
+		}
+	}
+
+	return k[packVolume-1][capability];
+}
+
+
 //출처 : https://www.hackerrank.com/challenges/common-child/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=strings
 
 int main()
