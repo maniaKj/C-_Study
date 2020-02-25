2ff6c89178ebfcc0c787aab5a48c5a6784051566
Jinpyo_K <39121365+maniaKj@users.noreply.github.com>
2020-02-25 20:38:52 +0900

    Update Item10.cpp

--- effectiveCPP/Item10.cpp
+++ effectiveCPP/Item10.cpp
@@ -4,11 +4,12 @@
 
 class Widget {
 public:
+	//대입 연산자는 *this의 참조자를 반환하게 하자
 	Widget& operator= (const Widget& rhs) {	//반환 타입은 현재의 클래스에 대한 참조자
 		//....
 		return *this;	//좌변 객체(의 참조자)를 반환
 	}
-	Widget& operator+=(const Widget& rhs) {	//다른 연산자에도 동일한 규약 적용
+	Widget& operator+=(const Widget& rhs) {	//다른 연산자(+=,-=, *= 등)에도 동일한 규약 적용
 		//....
 		return *this;
 	}
